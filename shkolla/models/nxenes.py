c0ae3de7506f412b62f0c23a3841cd5d41e6db9d
jeton <jetonstojku@hotmail.com>
2024-08-09 10:34:17 +0200

    add notat

--- shkolla/models/nxenes.py
+++ shkolla/models/nxenes.py
@@ -9,3 +9,14 @@ class ShkollaNxenes(models.Model):
     mbiemer = fields.Char(string="Mbiemer", required=True)
     ditelindje = fields.Date(string='Ditelindje', required=True)
     nr_amez = fields.Integer(string="Nr. Amez", required=True)
+    notat = fields.One2many(comodel_name='shkolla.notat', inverse_name='nxenes_id', string='Notat')
+
+
+class ShkollaNotat(models.Model):
+    _name = 'shkolla.notat'
+    _description = 'ShkollaNotat'
+
+    lenda = fields.Char(string="Lenda", required=True)
+    nxenes_id = fields.Many2one(comodel_name='shkolla.nxenes', string='Nxenes', required=True)
+    nota = fields.Integer(string="Notat", required=True)
+
